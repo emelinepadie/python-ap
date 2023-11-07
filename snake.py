03e36134be6db9dcaa6d803e24590c173441b1b5
Emeline Padié <emeline.padie@etu.minesparis.psl.eu>
2023-11-07 21:36:22 +0100

    j'ai ajouté les commentaires

--- snake.py
+++ snake.py
@@ -9,24 +9,26 @@ VERT = (0, 255, 0)
 LARGEUR = 20
 
 clock = pygame.time.Clock()
-hauteur = 20
+HAUTEUR = 20
 longueur = 20*3
 lefts = 5*20                    ## position du snake à t = 0
 tops = 10*20
-snake = [hauteur, longueur, lefts, tops]
+snake = [HAUTEUR, longueur, lefts, tops]
 
 while True:
 
     clock.tick(1)
-    
+
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_q:
-                pygame.quit()
-            #if event.key == pygame.K_
+                pygame.quit()                           ## raccourcit clavier pour arrêter le programme
+            #if event.key == pygame.K_                  ## déplacer le snake
     
-    screen.fill(WHITE)
+    screen.fill(WHITE)                                  ## création de l'écran vide blanc
     
+
+    ## création de l'échequier
     left = 0
     while left < 400 : 
         top = 0
@@ -45,6 +47,7 @@ while True:
             top += 40
         left += 40
 
+    ## création du serpent à t = 0
     rect = pygame.Rect(snake[2], snake[3], snake[1], snake[0])
     pygame.draw.rect(screen, VERT, rect)
 
