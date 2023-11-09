75394f6b2475cd2e54bb9b497879d523c2db709e
Emeline Padié <emeline.padie@etu.minesparis.psl.eu>
2023-11-09 09:30:06 +0100

    le snake se déplace seul

--- snake.py
+++ snake.py
@@ -15,43 +15,14 @@ lefts = 5*20                                                 ## position du snak
 tops = 10*20
 #snake = [HAUTEUR, longueur, lefts, tops]
 
-head = [10*20, 5*20]
-middle = [10*20, 6*20]
-tail = [10*20, 7*20]
-snake = [head, middle, tail]
+snake = ['right', [10*20, 5*20], [10*20, 6*20], [10*20, 7*20], 'right']
 
 while True:
 
     clock.tick(5)
 
-    for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_q:
-                pygame.quit()                               ## raccourcit clavier pour arrêter le programme
-            if event.key == pygame.K_LEFT :                 ## déplacer le snake vers la gauche
-                for i in range (2):
-                    middle[i] = head[i]
-                    tail[i] = middle[i]
-                head[1] -= 20             
-            if event.key == pygame.K_RIGHT :                ## déplacer le snake vers la droite
-                for i in range (2):
-                    middle[i] = head[i]
-                    tail[i] = middle[i]
-                head[1] += 20
-            if event.key == pygame.K_UP :                   ## déplacer le snake vers le haut
-                for i in range (2):
-                    middle[i] = head[i]
-                    tail[i] = middle[i]
-                head[0] -= 20
-            if event.key == pygame.K_DOWN :                 ## déplacer le snake vers le bas
-                for i in range (2):
-                    middle[i] = head[i]
-                    tail[i] = middle[i]
-                head[0] += 20
-    
-    
     screen.fill(WHITE)                                      ## création de l'écran vide blanc
-    
+
     ## création de l'échequier
     left = 0
     while left < 400 : 
@@ -72,12 +43,73 @@ while True:
         left += 40
 
     ## création du serpent à t = 0
-    rect = pygame.Rect(snake[0][1], snake[0][0], HAUTEUR, HAUTEUR)
-    pygame.draw.rect(screen, VERT, rect)
     rect = pygame.Rect(snake[1][1], snake[1][0], HAUTEUR, HAUTEUR)
     pygame.draw.rect(screen, VERT, rect)
     rect = pygame.Rect(snake[2][1], snake[2][0], HAUTEUR, HAUTEUR)
     pygame.draw.rect(screen, VERT, rect)
+    rect = pygame.Rect(snake[3][1], snake[3][0], HAUTEUR, HAUTEUR)
+    pygame.draw.rect(screen, VERT, rect)
+
+    ## déplacement du serpent
+
+    if snake[0] == 'right' : 
+        for i in range (1, len(snake) - 1):
+            for k in range (2) : 
+                snake[len(snake) - i][k] = snake[len(snake) - i - 1][k]
+        head[1] += 20
+    if snake[0] == 'left' : 
+        for i in range (1, len(snake) - 1):
+            for k in range (2) : 
+                snake[len(snake) - i][k] = snake[len(snake) - i - 1][k]
+        head[1] -= 20 
+    if snake[0] =='up' : 
+        for i in range (1, len(snake) - 1):
+            for k in range (2) : 
+                snake[len(snake) - i][k] = snake[len(snake) - i - 1][k]
+        head[0] -= 20 
+    if snake[0] == 'down' : 
+        for i in range (1, len(snake) - 1):
+            for k in range (2) : 
+                snake[len(snake) - i][k] = snake[len(snake) - i - 1][k]
+        head[0] += 20
+        
+
+
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_q:
+                pygame.quit()                               ## raccourcit clavier pour arrêter le programme
+            if event.key == pygame.K_LEFT :                 ## déplacer le snake vers la gauche
+                for i in range (1, len(snake) - 1):
+                    for k in range (2) : 
+                        snake[len(snake) - i][k] = snake[len(snake) - i - 1][k]
+                head[1] -= 20 
+                snake[0] = 'left'            
+            if event.key == pygame.K_RIGHT :                ## déplacer le snake vers la droite
+                for i in range (1, len(snake) - 1):
+                    for k in range (2) : 
+                        snake[len(snake) - i][k] = snake[len(snake) - i - 1][k]
+                head[1] += 20
+                snake[0] = 'right'
+            if event.key == pygame.K_UP :                   ## déplacer le snake vers le haut
+                for i in range (1, len(snake) - 1):
+                    for k in range (2) : 
+                        snake[len(snake) - i][k] = snake[len(snake) - i - 1][k]
+                head[0] -= 20
+                snake[0] = 'up'
+            if event.key == pygame.K_DOWN :                 ## déplacer le snake vers le bas
+                for i in range (1, len(snake) - 1):
+                    for k in range (2) : 
+                        snake[len(snake) - i][k] = snake[len(snake) - i - 1][k]
+                head[0] += 20
+                snake[0] = 'down'
+    
+    
+    ## faire apparaître le fruit
+    
+    
+
+    
 
 
     pygame.display.update()
