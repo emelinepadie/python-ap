2f13836567b958e70573143259d44ad1c1da5674
Emeline Padié <emeline.padie@etu.minesparis.psl.eu>
2023-12-12 16:27:33 +0100

    lecture du fichier fasta

--- sequence.py
+++ sequence.py
@@ -2,3 +2,13 @@
 import argparse
 import logging
 import os
+import sys
+
+## ouverture du fichier sur l'entrée standard
+for line in sys.stdin :
+    if line[0] == ';':
+        continue
+    else:
+        print(f'{line}', end = '')
+
+
