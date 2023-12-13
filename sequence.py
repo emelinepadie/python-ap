10e753fdf5da515293bd66a7cb42f69edd4b2373
Emeline Padié <emeline.padie@etu.minesparis.psl.eu>
2023-12-13 19:23:15 +0100

    sequence

--- sequence.py
+++ sequence.py
@@ -3,12 +3,28 @@ import argparse
 import logging
 import os
 import sys
+import re
 
 ## ouverture du fichier sur l'entrée standard
+seq_id = None
+seq = None
+var = None
+
 for line in sys.stdin :
-    if line[0] == ';':
+    if line[0] == ';' :
         continue
-    else:
-        print(f'{line}', end = '')
+    if line[0] == '>' and seq_id == None:
+        seq_id = line
+    elif line[0] == '>' and seq_id != None:
+        continue
+    if seq == None and line[0] != ';' and line[0] != '>' and line != []: ## faire ça avec match pour selectionner une ligne
+        seq = line
+    elif var == None and seq != None:
+        var = line
+        print(seq_id, seq, var)
+        seq_id = None
+        seq = None
+        var = None
+
 
 
