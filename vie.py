e49c18ae8f0c43a738d6be78048b7bb20f15d8e6
Emeline Padié <emeline.padie@etu.minesparis.psl.eu>
2023-12-28 19:20:48 +0100

    arguments

--- vie.py
+++ vie.py
@@ -6,6 +6,21 @@ import logging
 import os
 #######################
 
+##Arguments
+
+def read_arg():
+    
+    parser = argparse.ArgumentParser(description='Some description.')
+    parser.add_argument('-i', type = str,default = my_input_file.txt, help ="Takes a str. Set the path to the input file.")
+    parser.add_argument('-o',type = str, default = my_output_file.txt, help = "Takes a str. Set the path to the output file.")
+    parser.add_argument('-m',type = int, default = 20, help='Take an int. Number of step to run when display is off.')
+    parser.add_argument('-d',help='A flag. When True, enable pygame', action='store_true')
+    parser.add_argument('-f',type = int, default = 10, help='Take an int. Number of frame per second.')
+    parser.add_argument('--width',type = int,default = 800,  help='Take an int. Window width.')
+    parser.add_argument('--height',type = int, default = 600,  help='Take an int. Window height.')
+    
+    args = parser.parse_args()
+
 ##Classes
 
 #Cellule
@@ -15,7 +30,7 @@ class Cell:
     # le constructeur
     def __init__(self, state, neighboors, pos):
         # un objet cell a des attributs
-        # state = vivant ou mort
+        # state = vivant (1) ou mort (0)
         # neighboors = nbr de cellules vivantes autours d'elle
         # pos = coordonnées de la cellule
         self._state = state
@@ -56,4 +71,6 @@ class Display:
     
     def __repr__(self):
         return self._display
-    
+
+def evol(map):
+
